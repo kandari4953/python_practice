eb26cc6bc655dc50b7fcf2cc13255c355e86f592
kandari4953 <sindhurajkandari4953@gmail.com>
2026-09-07 11:21:59 +0530

    Add files via upload

---
+++ File_Handling.py
@@ -0,0 +1,19 @@
+# f = open("demofile.txt")
+# print(f.read())
+
+# f= open("D_backup\Python Practice\demofile.txt")
+# print(f.read())
+
+# with open ("demofile.txt", "w") as f:
+#     f.write("mmmmmmmmm")
+    
+# with open ("demofile.txt") as f:
+#     print(f.read())
+
+# f = open("myfile.txt", "a")
+
+# import os
+# if os.path.exists("myfile.txt"):
+#     os.remove("myfile.txt")
+# else:
+#     print("file doe not exist")
